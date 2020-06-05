57ca18d279923fff8990c9b86aef0e4aaadf6810
Gnatenko <gnatsenko@gmail.com>
2020-06-05 23:07:27 +0300

    complicated homework

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-let money = 10000;
-let	income = 5000;
-let	addExpenses = 'коммуналка, ТО автомобиля, кофе, уксус';
-let	deposit = true;
-let	mission = 1000000;
-let	period = 8;
-let budgetDay = money / 30;
+let num = 266219;
+let str = String(num)
+let arr = str.split('');
+let amount = 0;
 
 
-console.log( typeof(money) );
-console.log( typeof(income) );
-console.log( typeof(deposit) );
 
-console.log(addExpenses.length);
+for (let i = 0; i < arr.length; i++) {
+	 amount +=+ arr[i];
 
-console.log( 'Период равен ' + period + ' месяцев' );
+}
 
-console.log( addExpenses.toLocaleLowerCase().split(', ') );
+console.log(amount);
 
-console.log(budgetDay);
+let amountPower = amount ** 3; // Возвёл в степень
+
+console.log(amountPower);
+
+console.log( String(amountPower).slice(0,2) );
+
+
